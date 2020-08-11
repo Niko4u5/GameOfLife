c089eb81874af0cef074cfdf74896fbceea7b5cd
niko4u <niko4u@fam-email.de>
2020-08-11 20:28:32 +0200

    started writing the display.

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Viewer viewer = new Viewer();
+        viewer.window();
+    }
+}
--- src/Viewer.java
+++ src/Viewer.java
@@ -1,5 +1,32 @@
-public class Viewer {
-    public static void main(String[] args) {
+import javax.swing.*;
+import java.util.concurrent.TimeUnit;
 
+public class Viewer {
+    public void window() throws InterruptedException {
+        Game game = new Game(new boolean[][]{{false,true,false},
+                                             {false,true,false},
+                                             {false,true,false}});
+
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400,400);
+        frame.setFocusable(true);
+
+        JTable board = new JTable(game.board.length,game.board.length);
+        board.setVisible(true);
+
+
+        while (true){
+            game.step();
+            for (int i = 0; i < game.board.length; i++){
+                for (int j = 0; j < game.board.length; j++){
+                    board.setValueAt(game.board[i][j],i,j);
+                }
+            }
+            TimeUnit.SECONDS.sleep(2);
+
+
+        }
     }
 }
