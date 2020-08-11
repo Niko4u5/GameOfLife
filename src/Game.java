0d03aca4d75a90f44563e54c63a270a6b5ff8e66
niko4u <niko4u@fam-email.de>
2020-08-11 19:49:31 +0200

    finished Game object, which stores the board and applies the rules.

--- src/Game.java
+++ src/Game.java
@@ -1,44 +1,67 @@
 public class Game {
     boolean[][] board;
     boolean[][] newBoard;
+    int count;
 
-    Game(int height, int with){
-        board = new boolean[height][with];
+    Game(int x, int y){
+        board = new boolean[x][y];
     }
 
-    Game(boolean[][] newBoard) {
-        this.board = newBoard;
+    Game(boolean[][] setBoard) {
+        board = setBoard;
     }
 
     public boolean getBoard(int x, int y) {
         try {
             return board[x][y];
         }catch (ArrayIndexOutOfBoundsException e){
-            return false;
+            return (false);
         }
 
     }
 
     public int getNeighbors(int x, int y){
-        int count = 0;
-        if(getBoard(x - 1,y - 1))
-            count++;
-        if(getBoard(x - 1,y))
-            count++;
-        if(getBoard(x - 1,y + 1))
-            count++;
-        if(getBoard(x,y + 1))
-            count++;
-        if(getBoard(x,y - 1))
-            count++;
-        if(getBoard(x + 1,y - 1))
-            count++;
-        if(getBoard(x + 1,y))
-            count++;
-        if(getBoard(x + 1,y + 1))
-            count++;
+        count = 0;
+        if(getBoard((x - 1),(y - 1))) {
+            count++;}
+
+        if(getBoard((x - 1),y)){
+            count++;}
+
+        if(getBoard((x - 1),(y + 1))){
+            count++;}
+
+        if(getBoard(x,(y + 1))){
+            count++;}
+
+        if(getBoard(x,(y - 1))){
+            count++;}
+
+        if(getBoard((x + 1),(y - 1))){
+            count++;}
+
+        if(getBoard((x + 1),y)){
+            count++;}
+
+        if(getBoard((x + 1),(y + 1))){
+            count++;}
+
         return count;
     }
 
-
+    public void step(){
+        newBoard = new boolean[board.length][board.length];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                if(getNeighbors(i, j) == 2 && board[i][j]){
+                    newBoard[i][j] = true;
+                } else if(getNeighbors(i, j) == 3){
+                    newBoard[i][j] = true;
+                }else{
+                    newBoard[i][j] = false;
+                }
+            }
+        }
+        board = newBoard;
+    }
 }
--- src/Viewer.java
+++ src/Viewer.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Viewer {
     public static void main(String[] args) {
 
     }
