4faab05b1f6bf4b290d65d9a3b2c538c20d4d483
niko4u <niko4u@fam-email.de>
2020-08-12 13:43:38 +0200

    created a way to change cells by typing their position.

--- src/Viewer.java
+++ src/Viewer.java
@@ -1,8 +1,10 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Viewer {
+    Game game = new Game(5);
+
     public void window() throws InterruptedException {
-        Game game = new Game(5);
         game.board[0][1] = true;
         game.board[1][2] = true;
         game.board[2][0] = true;
@@ -12,22 +14,42 @@ public class Viewer {
 
         while (true){
             System.out.println();
-            for (int i = 0; i < game.board.length; i++){
+
+            for (int i = -1; i < game.board.length; i++){
                 for (int j = 0; j < game.board.length; j++){
-                    if (game.board[i][j]){
+                    if(i == -1){
+                        System.out.print(j + "  ");
+                    }else if (game.board[i][j]){
                         System.out.print("X  ");
                     }else System.out.print("_  ");
                 }
-                System.out.println();
-            }
-            try {
-                System.in.read();
-            } catch (IOException e) {
-                e.printStackTrace();
+                System.out.println(i);
             }
             game.step();
-
+            getInput();
 
         }
+    }
+
+    private void getInput(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("type y to change a cell");
+        if(scanner.next().contains("y")){
+            String string = scanner.next();
+            String[] Values = string.split(",");
+            ;
+            game.board[Integer.parseInt(Values[0])][Integer.parseInt(Values[0])] = !game.board[Integer.parseInt(Values[0])][Integer.parseInt(Values[0])];
+
+            System.out.println("Do you want to change another cell?");
+            while (scanner.next().contains("y")){
+                string = scanner.next();
+                Values = string.split(",");
+                ;
+                game.board[Integer.parseInt(Values[0])][Integer.parseInt(Values[0])] = !game.board[Integer.parseInt(Values[0])][Integer.parseInt(Values[0])];
+
+                System.out.println("Do you want to change another cell?");
+            }
+        }
+
     }
 }
